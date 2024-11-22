3b687c5777bd05efd35d2b1dc297a2faacc96733
Denis Tsariov <tsariov.den@gmail.com>
2024-11-22 18:58:12 -0500

    init2

---
+++ app/(tabs)/my-profile.tsx
@@ -0,0 +1,10 @@
+import { ThemedText } from "@/components/ThemedText";
+import { View, Text } from "react-native";
+
+export default function MyProfile() {
+  return (
+    <View className="h-full flex items-center justify-center">
+      <ThemedText>profile page</ThemedText>
+    </View>
+  );
+}
