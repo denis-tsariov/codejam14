21737402e1f26b52df0d7b787deb14073cb72385
sham-bolic <143747815+sham-bolic@users.noreply.github.com>
2024-11-23 11:32:33 -0500

    auth pages test

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -54,6 +54,14 @@ export default function TabLayout() {
           ),
         }}
       />
+      <Tabs.Screen  
+        name="login"
+        options={{ title: "Login" }}
+      />
+      <Tabs.Screen  
+        name="signup"
+        options={{ title: "SignUp" }}
+      />
     </Tabs>
   );
 }
--- app/(tabs)/my-profile.tsx
+++ app/(tabs)/my-profile.tsx
@@ -1,10 +1,32 @@
-import { ThemedText } from "@/components/ThemedText";
-import { View, Text } from "react-native";
+import { Link } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function MyProfile() {
   return (
-    <View className="h-full flex items-center justify-center">
-      <ThemedText>profile page</ThemedText>
+    <View style={styles.container}>
+      <Text>Welcome</Text>
+
+      {/* Link to the Login Modal */}
+      <Link href="/auth/login" style={styles.link}>
+        Login
+      </Link>
+
+      {/* Link to the Sign Up Modal */}
+      <Link href="/auth/signup" style={styles.link}>
+        Sign Up
+      </Link>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  link: {
+    paddingTop: 20,
+    fontSize: 20,
+  },
+});
