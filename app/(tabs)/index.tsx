02df87b8b09baf0f55f78d07fa0a37ee98b30faa
Denis Tsariov <tsariov.den@gmail.com>
2024-11-23 12:54:09 -0500

    swipe working nicely

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,46 +1,53 @@
-import { useState, useRef } from "react";
-import { View, StyleSheet } from "react-native";
+import { useRef } from "react";
+import { View } from "react-native";
 import DecisionButtons from "@/components/home/decision-buttons";
 import FoodPlane from "@/components/home/food-plane";
 import food from "../../assets/data/users";
+import Swiper from "react-native-deck-swiper";
 
 export default function HomeScreen() {
-  const [currentIndex, setCurrentIndex] = useState(0);
-  console.log(currentIndex);
-  const swipeRef = useRef<{
-    swipe: (direction: "left" | "right") => void;
-  } | null>(null);
+  const swiperRef = useRef<Swiper<any>>(null);
 
-  function handleSwipe(direction: "left" | "right") {
-    swipeRef.current?.swipe(direction);
-    // Move to next card after animation
-    setTimeout(() => {
-      setCurrentIndex((index) => index + 1);
-    }, 500);
-    console.log("update currentIndex", currentIndex);
-  }
+  const handleSwipe = (direction: "left" | "right") => {
+    if (!swiperRef.current) {
+      console.log("ayo?");
+      return;
+    }
+    if (direction === "left") {
+      console.log("swipe left");
+      swiperRef.current.swipeLeft();
+    } else {
+      console.log("swipe right");
+      swiperRef.current?.swipeRight();
+    }
+  };
 
   return (
-    <View className="w-full h-full flex flex-col items-center justify-center relative">
-      {currentIndex < food.length && (
-        <View className="w-11/12 relative" style={styles.thing}>
-          <FoodPlane
-            food={food[currentIndex]}
-            index={1}
-            ref={swipeRef}
-            handleSwipe={handleSwipe}
-            nextFood={food[currentIndex + 1]}
-          />
-          {currentIndex + 1 < food.length && (
-            <FoodPlane
-              food={food[currentIndex + 1]}
-              index={0}
-              handleSwipe={null}
-              nextFood={null}
-            />
-          )}
-        </View>
-      )}
+    <View className="h-full w-full relative">
+      <View className="absolute h-5/6 top-0 flex flex-col items-center justify-center bg-transparent">
+        <Swiper
+          cards={food}
+          renderCard={(card) => {
+            return <FoodPlane food={card} />;
+          }}
+          ref={swiperRef}
+          // stackSeparation={14}
+          // stackScale={0.95}
+          disableBottomSwipe
+          disableTopSwipe
+          showSecondCard={true}
+          cardIndex={0}
+          backgroundColor={"#FFFFFF"}
+          stackSize={3}
+          useViewOverflow={false}
+          onSwiped={(cardIndex) => {
+            console.log(cardIndex);
+          }}
+          onSwipedAll={() => {
+            console.log("onSwipedAll");
+          }}
+        />
+      </View>
       <DecisionButtons
         onSwipeLeft={() => handleSwipe("left")}
         onSwipeRight={() => handleSwipe("right")}
@@ -48,9 +55,3 @@ export default function HomeScreen() {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  thing: {
-    height: "66.66%",
-  },
-});
--- components/home/decision-buttons.tsx
+++ components/home/decision-buttons.tsx
@@ -2,7 +2,7 @@ import { View, Text } from "react-native";
 
 export default function DecisionButtons({ onSwipeLeft, onSwipeRight } : any) {
   return (
-    <View className="w-11/12 flex flex-row h-20 items-center justify-around">
+    <View className="absolute z-40 w-full flex flex-row h-20 items-center justify-around bottom-28">
       <View 
         className="flex justify-center items-center w-20 h-10 border bg-orange-900 rounded-xl"
         onTouchEnd={onSwipeLeft}
