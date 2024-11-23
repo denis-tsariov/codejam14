46ed80194a0a12c7a323ae980fad2b4521ce377e
greta_zu <149711822+zu-greta@users.noreply.github.com>
2024-11-23 15:53:48 -0500

    mb

--- api_call/db_calls.js
+++ api_call/db_calls.js
@@ -73,13 +73,31 @@ export const getRestaurantFoodImages = async (id) => {
     return await fetchData(`restaurants/food_array/${id}`);
   }
 
+// add a new record to the map table
+export const addMapRecord = async (data) => {
+    return await postData('maps', data);
+  }
+  
+  // update an existing record in the map table
+  export const updateMapRecord = async (data) => {
+    return await updateData('maps', data);
+  }
+  
+  // delete a record from the map table
+  export const deleteMapRecord = async (id) => {
+    return await deleteData(`maps/${id}`);
+  }
+
 
 // export all functions as a module
 export default {
     getRestaurants,
     getRestaurantsWithPagination,
     getRestaurantById,
-    getRestaurantFoodImages
+    getRestaurantFoodImages,
+    addMapRecord, 
+    updateMapRecord,
+    deleteMapRecord
   };
 
 
@@ -87,4 +105,5 @@ export default {
 functions we need:
 - get a list of restaurants: json files with all the info of the restaurants (name, cost, food_array, etc)
 - add a map record: add a new record to the map table with a given user_id and restaurant_id
+- get a list of map record for a certain user: get all the records in the map table for a given user_id
 */
